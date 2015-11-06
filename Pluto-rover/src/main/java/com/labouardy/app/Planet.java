a738d23e44357217073e2c6bde77e9ea49c1b1e5
mlabouardy <mohamed.labouardy@gmail.com>
2015-11-06 16:28:11 +0100

    each planet has a list of obstacles

--- Pluto-rover/src/main/java/com/labouardy/app/Planet.java
+++ Pluto-rover/src/main/java/com/labouardy/app/Planet.java
@@ -1,15 +1,35 @@
 package com.labouardy.app;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Planet {
 	private Character[][] grid;
 	private int n;
+	private List<Obstacle> obstacles;
 	
 	public Planet(int n){
 		this.n=n;
 		this.grid=new Character[n][n];
+		this.obstacles=new ArrayList<Obstacle>();
+	}
+	
+	public void addObstacle(Obstacle obstacle){
+		obstacles.add(obstacle);
+		int x=realX(obstacle.getX());
+		int y=realY(obstacle.getY());
+		grid[y][x]='O';
 	}
 	
 	public int getSize(){
 		return n;
+	}
+	
+	public int realX(int x){
+		return x;
+	}
+	
+	public int realY(int y){
+		return n-1-y;
 	}
 }
