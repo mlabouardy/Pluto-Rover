1e41e7d243fcf375c4abacf485a2b13ad9ad1975
mlabouardy <mohamed.labouardy@gmail.com>
2015-11-06 16:29:16 +0100

    If rover report an obstacle

---
+++ Pluto-rover/src/main/java/com/labouardy/exception/ObstacleDetectedException.java
@@ -0,0 +1,9 @@
+package com.labouardy.exception;
+
+public class ObstacleDetectedException extends Exception{
+	private static final long serialVersionUID = 1L;
+
+	public ObstacleDetectedException(String msg){
+		super(msg);
+	}
+}
