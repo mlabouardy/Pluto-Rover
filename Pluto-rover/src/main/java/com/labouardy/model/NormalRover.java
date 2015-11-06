49aeee88d9b3cfe77b50bcacb95878a77608d50b
mlabouardy <mohamed.labouardy@gmail.com>
2015-11-06 16:16:20 +0100

    forward() implemented

--- Pluto-rover/src/main/java/com/labouardy/model/NormalRover.java
+++ Pluto-rover/src/main/java/com/labouardy/model/NormalRover.java
@@ -46,9 +46,42 @@ public class NormalRover extends AbstractRover {
 			e.printStackTrace();
 		}
 	}
+	
+	public void forwardChecker(Planet planet) throws RoverStuckException{
+		int size=planet.getSize();
+		if(direction.compareTo(Compass.E)==0){
+			if(x==size-1){
+				throw new RoverStuckException("stuck !!");
+			}else{
+				x++;
+			}
+		}else if(direction.compareTo(Compass.W)==0){
+			if(x==0){
+				throw new RoverStuckException("stuck !!");
+			}else{
+				x--;
+			}
+		}else if(direction.compareTo(Compass.N)==0){
+			if(y==size-1){
+				throw new RoverStuckException("stuck !!");
+			}else{
+				y++;
+			}
+		}else if(direction.compareTo(Compass.S)==0){
+			if(y==0){
+				throw new RoverStuckException("stuck !!");
+			}else{
+				y--;
+			}
+		}
+	}
 
 	public void forward(Planet planet) {
-		
+		try {
+			forwardChecker(planet);
+		} catch (RoverStuckException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
