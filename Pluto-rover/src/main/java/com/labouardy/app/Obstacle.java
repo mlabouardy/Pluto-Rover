fcddcbab63f07a2303db84c3c56b6dc42ef1f12f
mlabouardy <mohamed.labouardy@gmail.com>
2015-11-06 16:25:49 +0100

    Represent an obstacle

---
+++ Pluto-rover/src/main/java/com/labouardy/app/Obstacle.java
@@ -0,0 +1,19 @@
+package com.labouardy.app;
+
+public class Obstacle {
+	private int x;
+	private int y;
+	
+	public Obstacle(int x, int y){
+		this.x=x;
+		this.y=y;
+	}
+	
+	public int getX(){
+		return x;
+	}
+	
+	public int getY(){
+		return y;
+	}
+}
